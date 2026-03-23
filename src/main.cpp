8528cf7fa635b6e54f29c85cc50dfb74212c8c38
Zeshan Rasul <zeshanrasul90@gmail.com>
2026-03-23 22:47:23 +0000

    Create vk Instance

--- src/main.cpp
+++ src/main.cpp
@@ -15,53 +15,92 @@ constexpr uint32_t HEIGHT = 1080;
 
 class HelloTriangleApplication {
 public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
 private:
-    void initVulkan() {
-    }
+	void initVulkan() {
+		createInstance();
+	}
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
 
-    void cleanup() {
-        glfwDestroyWindow(window);
+	void cleanup() {
+		glfwDestroyWindow(window);
 
-        glfwTerminate();
-    }
+		glfwTerminate();
+	}
 
-    void initWindow() {
-        glfwInit();
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Novus Engine", nullptr, nullptr);
-    }
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Novus Engine", nullptr, nullptr);
+	}
+
+	void createInstance() {
+		constexpr vk::ApplicationInfo appInfo{ .pApplicationName = "Novus Engine",
+											  .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+											  .pEngineName = "No Engine",
+											  .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+											  .apiVersion = vk::ApiVersion14 };
+
+		uint32_t glfwExtensionCount = 0;
+		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		auto extensionProperties = context.enumerateInstanceExtensionProperties();
+		for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+		{
+			if (std::ranges::none_of(extensionProperties,
+				[glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+				{ return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+			{
+				throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+			}
+		}
+
+		auto extensions = context.enumerateInstanceExtensionProperties();
+
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+
+		vk::InstanceCreateInfo createInfo{
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions };
+
+		instance = vk::raii::Instance(context, createInfo);
+	}
 
 private:
-    GLFWwindow* window;
+	GLFWwindow* window;
+
+	vk::raii::Context context;
+	vk::raii::Instance instance = nullptr;
 };
 
 int main()
 {
-    try
-    {
-        HelloTriangleApplication app;
-        app.run();
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try
+	{
+		HelloTriangleApplication app;
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
